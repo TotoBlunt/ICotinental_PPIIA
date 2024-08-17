4149ae5045232178a6a7f3951cc16f4e5bf4cc29
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-08-16 20:30:43 -0500

    leer el archivo

--- semana08.py
+++ semana08.py
@@ -6,3 +6,10 @@ st.title("K-Means Clustering con Streamlit")
 
 #Subir archivo de excel
 upload_file = st.file_uploader('Sube un archivo Excel',type=['xlsx'])
+
+if upload_file is not None:
+    #Leer archivo excel
+    df = pd.read_excel(upload_file)
+
+    st.write('### Vista previa de los datos')
+    st.write(df.head())
