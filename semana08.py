94c255592bb5f8681296e370eba3476390eb930e
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-08-16 20:26:32 -0500

    cargando archivos

--- semana08.py
+++ semana08.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 #Titulo para el app
 st.title("K-Means Clustering con Streamlit")
+
+#Subir archivo de excel
+upload_file = st.file_uploader('Sube un archivo Excel',type=['xlsx']
