44e3360e2e1e1a9e1c9a2b4479290d9f2c0aeb3c
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-08-16 20:36:02 -0500

    agregando conversion dummies

--- semana08.py
+++ semana08.py
@@ -13,3 +13,17 @@ if upload_file is not None:
 
     st.write('### Vista previa de los datos')
     st.write(df.head())
+
+    #Seleccionar columnas categoricas
+    categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+
+    if categorical_columns:
+        st.write('### Columnas categoricas indentificadas')
+        st.write(categorical_columns)
+
+        #Convertir columnas categoricas a dummies
+        df = pd.get_dummies(df,columns=categorical_columns)
+        st.write('### Datos despues de la conversion a dummies')
+        st.write(df.head())
+    else:
+        st.write('No se encontraron columnas categoricas en los datos')
