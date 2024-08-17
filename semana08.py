fdab771e823d9761971739b7cf8d98b9c41d211f
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-08-16 20:40:37 -0500

    añadiendo excepciones

--- semana08.py
+++ semana08.py
@@ -8,22 +8,25 @@ st.title("K-Means Clustering con Streamlit")
 upload_file = st.file_uploader('Sube un archivo Excel',type=['xlsx'])
 
 if upload_file is not None:
-    #Leer archivo excel
-    df = pd.read_excel(upload_file)
+    try: 
+        #Leer archivo excel
+        df = pd.read_excel(upload_file)
 
-    st.write('### Vista previa de los datos')
-    st.write(df.head())
-
-    #Seleccionar columnas categoricas
-    categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
-
-    if categorical_columns:
-        st.write('### Columnas categoricas indentificadas')
-        st.write(categorical_columns)
-
-        #Convertir columnas categoricas a dummies
-        df = pd.get_dummies(df,columns=categorical_columns)
-        st.write('### Datos despues de la conversion a dummies')
+        st.write('### Vista previa de los datos')
         st.write(df.head())
-    else:
-        st.write('No se encontraron columnas categoricas en los datos')
+
+        #Seleccionar columnas categoricas
+        categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+
+        if categorical_columns:
+            st.write('### Columnas categoricas indentificadas')
+            st.write(categorical_columns)
+
+            #Convertir columnas categoricas a dummies
+            df = pd.get_dummies(df,columns=categorical_columns)
+            st.write('### Datos despues de la conversion a dummies')
+            st.write(df.head())
+        else:
+            st.write('No se encontraron columnas categoricas en los datos')
+    except Exception as e:
+        st.error(f'Error al leer archivo excel: {e}')
